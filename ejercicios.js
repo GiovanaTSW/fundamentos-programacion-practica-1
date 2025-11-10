f11f15ed7631480f6666d8d1f2986bbb27892de9
GiovanaTSW <giovana.diaz@tecdesoftware.edu.mx>
2025-11-10 17:53:10 -0600

    Ejercicio 1.4 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -61,7 +61,7 @@ function areaRectangulo(base, altura) {
  */
 function celsiusAFahrenheit(celsius) {
   // TODO: Implementa la conversión
-  return F = celsius * 9/5 + 32;
+  return celsius * 9/5 + 32;
 }
 
 // ============================================
