eba0ecdb9b232ec6a5aa318dc441fefb8ba3b2e7
GiovanaTSW <giovana.diaz@tecdesoftware.edu.mx>
2025-11-11 07:33:53 -0600

    Ejercicio 3.4 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -174,13 +174,17 @@ function tablaMultiplicar(numero) {
 
 /**
  * Ejercicio 3.4: Números pares hasta n (6 puntos)
- * @param {number} n - NÃºmero lÃ­mite
- * @returns {Array} - Array con todos los nÃºmeros pares desde 2 hasta n
+ * @param {number} n - Número lí­mite
+ * @returns {Array} - Array con todos los números pares desde 2 hasta n
  * Ejemplo: numerosPares(10) debe retornar [2, 4, 6, 8, 10]
  */
 function numerosPares(n) {
   // TODO: Crea un array con todos los nÃºmeros pares hasta n
-
+  const pares = []
+  for(let i = 2; i <= n; i += 2) {
+    pares.push(i);
+  }
+  return pares;
 }
 
 // ============================================
